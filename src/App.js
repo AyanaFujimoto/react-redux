4d2f66c81ddc1d5a9e0d5f9b058f323a95a2256c
AyanaFujimoto <ayanba.fujimoto01@g.softbank.co.jp>
2020-06-08 18:46:37 +0900

    create class components and functional components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from "react";
 
-function App() {
-  // const greeting = "Hi, Dom!";
-  // const dom = <h1 className="foo">{greeting}</h1>;
-  // return <h1>Hello World!</h1>;
-  // return dom;
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        // onClick={() => {
-        onChange={() => {
-          console.log("I am clicked!");
-        }}
-      />
-    </React.Fragment>
-  );
-}
+// function App() {
+//   // const greeting = "Hi, Dom!";
+//   // const dom = <h1 className="foo">{greeting}</h1>;
+//   // return <h1>Hello World!</h1>;
+//   // return dom;
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         // onClick={() => {
+//         onChange={() => {
+//           console.log("I am clicked!");
+//         }}
+//       />
+//     </React.Fragment>
+//   );
+// }
 
 // function App() {
 //   return React.createElement("div", null, "Hello World!!");
 // }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
