7d550c47d92c0dcf8e601f93ce6f3fd5ed3d72de
AyanaFujimoto <ayanba.fujimoto01@g.softbank.co.jp>
2020-06-08 19:33:04 +0900

    learn state and setaState

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
 
-const App = () => {
-  const profiles = [
-    { name: "Aya", age: 25 },
-    { name: "Cookie", age: 5 },
-    { name: "NoName" },
-    { name: "41", age: 5 },
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
+const App = () => <Counter></Counter>;
 
-const User = (props) => {
-  return (
-    <div>
-      Hi. Iam {props.name}, and {props.age} years old.
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-};
+  handlePlusButton = () => {
+    console.log("handlePlusButton");
+    // console.log(this.state.count);
+    // const count = this.state.count;
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMinusButton = () => {
+    console.log("handleMinusButton");
+    // const count = this.state.count;
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    console.log("render");
+    // console.log(this.state.count);
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
