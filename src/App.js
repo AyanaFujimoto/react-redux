cb858c6045c3b22824e99d4f74fd5539f6ad5282
AyanaFujimoto <ayanba.fujimoto01@g.softbank.co.jp>
2020-06-08 16:49:33 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 function App() {
+  // const greeting = "Hi, Dom!";
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return <h1>Hello World!</h1>;
+  // return dom;
   return (
-    <div>
-      <h1> Hello World!</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input
+        type="text"
+        // onClick={() => {
+        onChange={() => {
+          console.log("I am clicked!");
+        }}
+      />
+    </React.Fragment>
   );
 }
+
+// function App() {
+//   return React.createElement("div", null, "Hello World!!");
+// }
 
 export default App;
