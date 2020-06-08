5f635eb5d88e1cda77fafc384c47ea11ad5f535c
AyanaFujimoto <ayanba.fujimoto01@g.softbank.co.jp>
2020-06-08 19:00:26 +0900

    learn props of coompoonents

--- src/App.js
+++ src/App.js
@@ -24,19 +24,30 @@ import React from "react";
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "Aya", age: 25 },
+    { name: "Cookie", age: 5 },
+    { name: "NoName" },
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!</div>;
+const User = (props) => {
+  return (
+    <div>
+      Hi. Iam {props.name}, and {props.age} years old.
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1,
 };
 
 export default App;
